1a3695acdfe1e8cf86ed31c3c91cf6ae05e37ec0
jillian <kjillian2025@gmail.com>
2026-01-23 21:38:23 -0600

    update

---
+++ controllers/users.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+
+
+const User = require('../models/user.js');
+
+router.get('/sign-up', (req, res) => {
+    res.render('auth/sign-up.ejs');
+});
+
+
+
+router.get('/sign-out', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+
+});
+
+
+router.get('/sign-in', (req, res) => {
+    res.render('auth/sign-in.ejs');
+});
+
+
+router.post('/sign-up', async (req, res) => {
+    try {
+        const userInDatabase = await User.findOne({ username: req.body.username });
+        if (userInDatabase) {
+            return res.send('Username taken.');
+        }}
+    });
+
+
+     if (req.body.password !== req.body.confirmPassword) {
+        return res.send('Password and confirm password must match');
+     }
+
+
+     const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+     req.body.password - hashedPassword;
+
+     await User.create(req.body);
+
+     res.redirect('/auth/sign-in');
+
+     router.post('/sign-in', async (req, res) => {
+        try {
+            const userInDatabase = await User.findOne({ username: req.body.username });
+            if (!userInDatabase) {
+                return res.send('Login failed.');
+            }
+        };
+
+         
+
+            const validPassword = bcrypt.compareSync(
+                req.body.password,
+                userInDatabase.password
+            );
+
+            if (!validPassword) {
+                return res.send('Login failed.');
+            }
+
+
+            req.session.user = {
+                username: userInDatabase.username,
+                _id: userInDatabase._id
+            };
+
+
+            res.redirect(`/user/${req.session.user._id}/foods`); 
+        } catch (error) {
+        res.redirect('/');
+        });
+            
+module.exports = router;
